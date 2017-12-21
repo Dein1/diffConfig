dfb954e27c7559ad715e232a50f8a772fb0bbf88
Dmitry Abramov <dmitry@MacBook-Pro-Dmitrij.local>
2017-12-21 13:46:26 +0300

    add recursive comparasion

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,50 +1,71 @@
-import { diff, compare } from '../src';
+import diff from '../src';
 
-test('flat json files', () => {
-  const config1 = '__tests__/__fixtures__/before.json';
-  const config2 = '__tests__/__fixtures__/after.json';
-  expect(diff(config1, config2)).toMatch(`{
+const flatResult = `{
     host: hexlet.io
   + timeout: 20
   - timeout: 50
   - proxy: 123.234.53.22
   + verbose: true
-}`);
+}`;
+
+test('flat json files', () => {
+  const config1 = '__tests__/__fixtures__/before.json';
+  const config2 = '__tests__/__fixtures__/after.json';
+  expect(diff(config1, config2)).toMatch(flatResult);
 });
 
 test('flat yaml files', () => {
   const config1 = '__tests__/__fixtures__/before.yml';
   const config2 = '__tests__/__fixtures__/after.yml';
-  expect(diff(config1, config2)).toMatch(`{
-    host: hexlet.io
-  + timeout: 20
-  - timeout: 50
-  - proxy: 123.234.53.22
-  + verbose: true
-}`);
+  expect(diff(config1, config2)).toMatch(flatResult);
 });
 
 test('flat ini files', () => {
   const config1 = '__tests__/__fixtures__/before.ini';
   const config2 = '__tests__/__fixtures__/after.ini';
-  expect(diff(config1, config2)).toMatch(`{
-    host: hexlet.io
-  + timeout: 20
-  - timeout: 50
-  - proxy: 123.234.53.22
-  + verbose: true
-}`);
+  expect(diff(config1, config2)).toMatch(flatResult);
 });
 
-test('compare - ast', () => {
-  const data1 = { host: 'hexlet.io', timeout: 50, proxy: '123.234.53.22' };
-  const data2 = { timeout: 20, verbose: true, host: 'hexlet.io' };
-  expect(compare(data1, data2)).toEqual([
-    { name: 'host', value: 'hexlet.io', action: 'unchanged' },
-    { name: 'timeout', value: 20, action: 'added' },
-    { name: 'timeout', value: 50, action: 'removed' },
-    { name: 'proxy', value: '123.234.53.22', action: 'removed' },
-    { name: 'verbose', value: true, action: 'added' },
-  ]);
+const recursiveResult = `{
+    common: {
+        setting1: Value 1
+      - setting2: 200
+        setting3: true
+      - setting6: {
+            key: value
+        }
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+    }
+    group1: {
+      + baz: bars
+      - baz: bas
+        foo: bar
+    }
+  - group2: {
+        abc: 12345
+    }
+  + group3: {
+        fee: 100500
+    }
+}`;
+
+test('recursive json files', () => {
+  const config1 = '__tests__/__fixtures__/before-recursive.json';
+  const config2 = '__tests__/__fixtures__/after-recursive.json';
+  expect(diff(config1, config2)).toMatch(recursiveResult);
 });
 
+test('recursive yaml files', () => {
+  const config1 = '__tests__/__fixtures__/before-recursive.yml';
+  const config2 = '__tests__/__fixtures__/after-recursive.yml';
+  expect(diff(config1, config2)).toMatch(recursiveResult);
+});
+
+test('recursive ini files', () => {
+  const config1 = '__tests__/__fixtures__/before-recursive.ini';
+  const config2 = '__tests__/__fixtures__/after-recursive.ini';
+  expect(diff(config1, config2)).toMatch(recursiveResult);
+});
--- src/index.js
+++ src/index.js
@@ -10,46 +10,66 @@ const parseMethods = {
   '.ini': ini.parse,
 };
 
-const parse = (file, extension) => {
-  const method = parseMethods[extension];
-  return method(file);
+const parseData = (file, extension) => {
+  const parse = parseMethods[extension];
+  return parse(file);
 };
 
 export const compare = (parsedData1, parsedData2) => {
   const allKeys = _.union(_.keys(parsedData1), _.keys(parsedData2));
   const compared = allKeys.reduce((acc, el) => {
+    if (_.isObject(parsedData1[el]) && _.isObject(parsedData2[el])) {
+      return [...acc, { name: el, value: compare(parsedData1[el], parsedData2[el]), type: 'recursive' }];
+    }
     if (parsedData1[el] === parsedData2[el]) {
-      return acc.concat({ name: el, value: parsedData2[el], action: 'unchanged' });
+      return [...acc, { name: el, value: parsedData2[el], type: 'unchanged' }];
     }
-    if (parsedData1[el] && parsedData2[el] === undefined) {
-      return acc.concat({ name: el, value: parsedData1[el], action: 'removed' });
+    if (!parsedData2[el]) {
+      return [...acc, { name: el, value: parsedData1[el], type: 'removed' }];
     }
-    if (parsedData1[el] === undefined) {
-      return acc.concat({ name: el, value: parsedData2[el], action: 'added' });
+    if (!parsedData1[el]) {
+      return [...acc, { name: el, value: parsedData2[el], type: 'added' }];
     }
-    return acc.concat({ name: el, value: parsedData2[el], action: 'added' }, { name: el, value: parsedData1[el], action: 'removed' });
+    return [...acc, { name: el, value: parsedData2[el], type: 'added' }, { name: el, value: parsedData1[el], type: 'removed' }];
   }, []);
   return compared;
 };
 
-const renderToString = (ast) => {
-  const reduced = ast.reduce((acc, el) => {
-    if (el.action === 'unchanged') {
-      return `${acc}    ${el.name}: ${el.value}\n`;
+const addPadding = level => '    '.repeat(level);
+
+const objectToString = (object, level) => {
+  const allKeys = Object.keys(object);
+  const reduced = allKeys.reduce((acc, el) => {
+    if (_.isObject(object[el])) {
+      return `${acc}${addPadding(level)}    ${el}: ${objectToString(object[el], level + 1)}\n`;
     }
-    if (el.action === 'removed') {
-      return `${acc}  - ${el.name}: ${el.value}\n`;
-    }
-    return `${acc}  + ${el.name}: ${el.value}\n`;
+    return `${acc}${addPadding(level)}    ${el}: ${object[el]}\n`;
   }, '{\n');
-  return `${reduced}}`;
+  return `${reduced}${addPadding(level)}}`;
 };
 
-export const diff = (filePath1, filePath2) => {
+const renderToString = (ast, level = 0) => {
+  const reduced = ast.reduce((acc, el) => {
+    if (el.type === 'recursive') {
+      return `${acc}${addPadding(level)}    ${el.name}: ${renderToString(el.value, level + 1)}\n`;
+    }
+    const newValue = _.isObject(el.value) ? objectToString(el.value, level + 1) : el.value;
+    if (el.type === 'unchanged') {
+      return `${acc}${addPadding(level)}    ${el.name}: ${newValue}\n`;
+    }
+    if (el.type === 'removed') {
+      return `${acc}${addPadding(level)}  - ${el.name}: ${newValue}\n`;
+    }
+    return `${acc}${addPadding(level)}  + ${el.name}: ${newValue}\n`;
+  }, '{\n');
+  return `${reduced}${addPadding(level)}}`;
+};
+
+export default (filePath1, filePath2) => {
   const file1 = fs.readFileSync(filePath1, 'utf8');
   const file2 = fs.readFileSync(filePath2, 'utf8');
-  const parsedData1 = parse(file1, path.extname(filePath1));
-  const parsedData2 = parse(file2, path.extname(filePath2));
+  const parsedData1 = parseData(file1, path.extname(filePath1));
+  const parsedData2 = parseData(file2, path.extname(filePath2));
   const compared = compare(parsedData1, parsedData2);
   return renderToString(compared);
 };
